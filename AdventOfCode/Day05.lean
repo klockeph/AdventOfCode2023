ad1cb39f3b62aa9456a7cae8f8ea4acd0943f6bc
Philipp Klocke <Phil_K97@gmx.de>
2023-12-06 21:44:04 +0100

    Worked a little on  Day5

--- AdventOfCode/Day05.lean
+++ AdventOfCode/Day05.lean
@@ -38,13 +38,27 @@ partial def readMaps : (List String) → (Nat → Nat) → (Nat → Nat)
 
 -- #eval (readMaps ["light:", "50 98 2", "", "field:", "0 15 37"] id) 51
 
+def minimumSeed (s : List Nat) (m : Nat → Nat) : Option Nat :=
+  (s.map m).minimum?
+
 def solve_one (l : List String) : Nat :=
   let s := readSeeds (l.get! 0)
   let m := readMaps (l.drop 2) id
-  match (s.map m).minimum? with
+  match minimumSeed s m with
   | Option.none => panic! "No minimum? Where are my seeds?!"
   | Option.some x => x
+
+def seedsToIntervals : List Nat → List (Nat × Nat)
+  | [] => []
+  | x::y::s => (x, x + y)::seedsToIntervals s
+  | _ => panic! "List length must be even"
+
+/-
+A possible idea for day two could be to coalesce the maps not into Num → Num but instead Range → Range.
+In that case we could then figure out what ranges the seed ranges are mapped to and easily find the minimum.
+-/
 
 def main : IO Unit := do
   let f ← IO.readInputForDay 5
   IO.println s!"Solution One: {solve_one $ (f.splitOn "\n").map String.trim}"
+  IO.println s!"Solution Two: todo"
